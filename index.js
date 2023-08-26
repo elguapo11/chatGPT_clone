a88c2f6ca1809cf85756b3aebebfb47e4b175d46
Benny Mendoza <bennybc9387@gmail.com>
2023-08-26 01:07:38 -0600

    created useless function

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
-const chatCompletion = openai.chat.completions
-  .create({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'user',
-        assistant: 'Greetings',
-      },
-    ],
-  })
-  .then((res) => {
-    console.log(res.choices[0].message.content);
-    console.log(`${res.usage.total_tokens} ` + 'number of tokens');
-  });
+const chatCompletion = function chat(reply) {
+  openai.chat.completions
+    .create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: `${reply}`,
+        },
+      ],
+    })
+    .then((res) => {
+      console.log(res.choices[0].message.content);
+      console.log(`${res.usage.total_tokens} ` + `/n` + 'number of tokens');
+    });
+};
+
+chatCompletion('create a simple function to demonstrate javascript');
