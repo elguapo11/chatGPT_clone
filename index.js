259f84c86f8f6042eb1962dfa711d2ae687babb4
Benny Mendoza <bennybc9387@gmail.com>
2023-08-26 20:08:09 -0600

    created second chat object

--- index.js
+++ index.js
@@ -7,10 +7,28 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
+function chat(response) {
+  openai.chat.completions
+    .create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: `${response}`,
+        },
+      ],
+    })
+    .then((res) => {
+      console.log(res.choices[0].message.content);
+      console.log(`${res.usage.total_tokens} ` + '' + 'number of tokens');
+    });
+}
+
 const chatCompletion = function chat(reply) {
   openai.chat.completions
     .create({
       model: 'gpt-3.5-turbo',
+      max_tokens: 30,
       messages: [
         {
           role: 'user',
@@ -24,4 +42,5 @@ const chatCompletion = function chat(reply) {
     });
 };
 
-chatCompletion('hello');
+chat('test');
+chatCompletion('test 2');
